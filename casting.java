d7ab6d75158d842345956bafcd8a8f18c8e94c0c
Israel <israelsiqueira04@gmail.com>
2025-08-15 11:40:45 -0300

    Adiciona NovoArquivo.java

---
+++ casting.java
@@ -0,0 +1,15 @@
+public class casting {
+    public static void main(String[] args){
+        double resultado = 0.0;
+        int resultado = (int) resultado;
+
+
+        int meuInt = 10;
+        double meuDouble= meuInt;
+
+        String meuString = "10";
+        int meuInt2 = Integer.parseInt(meuString);
+
+        String minhaString = String.valueOf(meuInt2);
+    }
+}
